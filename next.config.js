b0620111b4509f6609a93e0a87dae6b33634f1f4
Apaar Seth <apaarseth007@gmail.com>
2023-02-21 01:25:02 +0530

    feat(travel-planner)/ initial impementation

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  env: {
-    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-  }
-}
-
-module.exports = nextConfig
